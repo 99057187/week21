1be57e024058064df60c1043480953881db06d76
anthony <99057187@mydavinci.nl>
2020-02-14 14:24:35 +0100

    no message

--- week21/B3W21O1 goedemorgen.php
+++ week21/B3W21O1 goedemorgen.php
@@ -1,28 +1,31 @@
 <?php
 	date_default_timezone_set("Europe/Amsterdam");
-	$time= date("H");
+	$time= date("H:i:s");
 	$outputtekst="";
 	$imgageScr="";
 	if ($time <6) {
 		$outputtekst="Goede Nacht";
-		$imageScr="night.png";
+		$imageScr="foto's/night.png";
 	}
 	else if ($time >=6 && $time<12 ) {
 		$outputtekst="Goede morgen";
-		$imageScr="morning.png";
+		$imageScr="foto's/morning.png";
 	}
 	else if ($time >=12 && $time<18) {
 		$outputtekst="Goede middag";
-		$imageScr="afternoon.png";
+		$imageScr="foto's/afternoon.png";
 	}
 	else if ($time >=18) {
 		$outputtekst="Goede avond";
-		$imageScr="evening.png";
+		$imageScr="foto's/evening.png";
 	}
+	$page = $_SERVER['PHP_SELF'];
+	$sec = "1";
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
 		<link rel="stylesheet" type="text/css" href="B3W21O1 goedemorgen.css">
 		<title><?=$outputtekst?></title>
 	</head>
@@ -30,7 +33,7 @@
 			<img src="<?=$imageScr?>" id="background">
 			<div id="plaats">
 				<h1><?=$outputtekst?></h1>
-				<h1><?= "het is nu " . date("H:i")?></h1>
+				<h1><?= "het is nu " . date("H:i:s")?></h1>
 			</div>
 	</body>
 </html>
